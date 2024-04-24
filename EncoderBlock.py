d276fd46b7e155b24ff7b846b60bd6b520b6c4c8
developer <developer@tinynets.io>
2024-04-24 17:33:23 -0400

    scratch posenc works

--- EncoderBlock.py
+++ EncoderBlock.py
@@ -16,11 +16,17 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         x = self.embedding(x)
+        print("Shape after embedding:", x.shape)
         x = self.pos_enc(x)
+        print("Shape after positional encoding:", x.shape)
         mha_output, attn_weights = self.mha(x)
+        print("Shape after multi-head attention:", mha_output.shape)
         x = self.addnorm(x, mha_output)
+        print("Shape after add-norm:", x.shape)
         x = self.ffn(x)
+        print("Shape after position-wise feed forward:", x.shape)
         x = self.addnorm2(x, x)
+        print("Shape after second add-norm:", x.shape)
         return x
         
         
