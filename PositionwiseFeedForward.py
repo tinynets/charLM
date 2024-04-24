ba7374de2bea57fc1eea86fb032b49f0baf7f0d5
developer <developer@tinynets.io>
2024-04-24 14:37:15 -0400

    positionwise feed forward layer implemented and added

---
+++ PositionwiseFeedForward.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.linear2(self.dropout(self.relu(self.linear1(x))))
