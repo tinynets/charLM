240b15186d19b8ba92b5ea70dbc396fb3b39e58c
developer <developer@tinynets.io>
2024-04-24 12:55:36 -0400

    AddNorm Done, on to FeedForward

---
+++ AddNorm.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class AddNorm(nn.Module):
+    def __init__(self, size, eps=1e-6):
+        super(AddNorm, self).__init__()
+        self.size = size
+        self.norm = nn.LayerNorm(size)
+        
+    
+    def forward(self, x):
+        return x + self.norm(x)
+    
