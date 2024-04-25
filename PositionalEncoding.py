89fe5c67deef44127cc714c689035f66d1246346
developer <developer@tinynets.io>
2024-04-24 20:34:33 -0400

    fixed shape issues and encoder done

--- PositionalEncoding.py
+++ PositionalEncoding.py
@@ -20,5 +20,10 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         seq_len = x.size(1)
-        return self.pos_encoding[:seq_len, :].unsqueeze(0)
+        pos_enc = self.pos_encoding[:seq_len, :].unsqueeze(0)
+        pos_enc = self.penc[:seq_len, :].unsqueeze(0)
+        return x + pos_enc
+
+    
+
 
