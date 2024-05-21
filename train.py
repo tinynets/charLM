4fea6cc3f5b222fa452c8008484d2e4b1d4eacc0
developer <developer@tinynets.io>
2024-05-21 00:05:24 -0400

    trainingabstractins and finally looking at some numbers

--- train.py
+++ train.py
@@ -1,91 +1,121 @@
+"""
+train.py
+
+This script is used to train a Transformer model on a given dataset. It includes functions to create the model 
+and to train it. The training function uses the Adam optimizer and CrossEntropyLoss as the loss function.
+
+Functions:
+- make_model: Creates a Transformer model with given parameters.
+- train_model: Trains the model on a given dataset for a specified number of epochs.
+
+Usage:
+- Import this module in your main script and call the functions with appropriate arguments.
+"""
+
 import torch
+from torch.utils.tensorboard import SummaryWriter
 import os
 from torch.utils.data import DataLoader
 from torch import optim
-from utils.utils import load_data, create_vocab, preprocess
-from utils.tokenizer import Tokenizer
+from utils import load_data, create_vocab, preprocess, split_data, Tokenizer
 from transformer_blocks import Transformer
 from utils.dataset_utils import CharDataset
 import datetime
 import time
 
 
-device = torch.device("mps")
-if str(device) != "mps":
-    raise ValueError("Invalid device. Only 'mps' is supported.")
+def make_model(vocab_size, d_model=64, seq_len=10, num_heads=8, device="mps"):
+    '''
+    Make the model and put it in the gpu and return it
+    '''
+    model = Transformer(vocab_size=vocab_size, d_model=d_model, n_decoder_layers=6, n_encoder_layers=6, num_heads=num_heads)
+    model.to(device)
+    return model
 
-timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-model_output_path = f'models/model_{timestamp}.pth'
+def train_model(dataset, model,
+    epochs=10, 
+    device="mps", 
+    batch_size=64, model_name="noname.pth"):
 
+    writer = SummaryWriter()
+    
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    total_batches = len(dataloader)
 
-batch_size = 64
-d_model = 64
-seq_len = 10
-num_heads = 8
+    model.to(device)
 
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+
+    print(f"Training model {model_name} started at {timestamp}")
+
+    epoch_end_times = []
+
+    
+
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}/{epochs}")
+        model.train()
+        total_loss = 0
+        start_time = time.time()
+        for batch, (src, tgt) in enumerate(dataloader):
+            print(f"Batch {batch+1}/{total_batches} of epoch {epoch+1}/{epochs} total_loss: {total_loss}")
+            src, tgt = src.to(device), tgt.to(device)
+            optimizer.zero_grad()
+            logits = model(src, tgt)
+
+            loss = criterion(logits.view(-1, logits.size(-1)), tgt.view(-1))
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            writer.add_scalar("Loss/train", loss.item(), epoch*total_batches + batch)
+        writer.flush()
+        epoch_time = time.time() - start_time
+        epoch_end_times.append(epoch_time)
+        print(f"Epoch {epoch+1}/{epochs} loss: {total_loss} time: {epoch_time}")
+        
+    
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    model_name = f"{timestamp}_{model_name}.pth"
+    save_path = os.path.join("./models", model_name)
+    
+    
+    torch.save(model.state_dict(), save_path)
+    print(f"Model saved at {save_path}")
+
+    trained_model = model
+
+    return trained_model
 
 data = load_data("data/input.txt")
 preprocessed_data = preprocess(data)
 vocab, vocab_size = create_vocab(preprocessed_data)
 
-# just using 2 batches for now to make sure it goes through the entire network
-preprocessed_data = preprocessed_data[:batch_size * 2]
+train_ds, test_ds, val_ds = split_data(preprocessed_data, [0.8, 0.1, 0.1])
 
+# variables needed for making model
+d_model = 64
+seq_len = 10   
+num_heads = 8
+
+model = make_model(vocab_size=vocab_size)
 
 tokenizer = Tokenizer(vocab)
 
-X = []
-Y = []
+train_ds = CharDataset(train_ds, tokenizer, seq_len)
+test_ds = CharDataset(test_ds, tokenizer, seq_len)
+val_ds = CharDataset(val_ds, tokenizer, seq_len)
 
+
+# setup dataset
+
+tokenizer = Tokenizer(vocab)
 dataset = CharDataset(preprocessed_data, tokenizer, seq_len)
 
-tokenized_data = tokenizer.encode(preprocessed_data)
 
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-
-
-# some logic to see what is inside a batch
-for batch_idx, (src, tgt) in enumerate(dataloader):
-    if batch_idx < 2:
-        print(f"Batch {batch_idx + 1}:")
-        print("Source:", src.shape)
-        print("Target:", tgt.shape)
-        # print(src[0])
-    else:
-        break
+model = train_model(dataset, model, epochs=2, model_name="transformer.pth", batch_size=128)
 
 
-model = Transformer(vocab_size=vocab_size, d_model=d_model, n_decoder_layers=6, n_encoder_layers=6, num_heads=num_heads)
-model.to(device)
 
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-total_batches = len(dataloader)
-
-
-start_time = time.time()    
-epoch_times = []
-
-for epoch in range(2):
-    print('epoch:', epoch)
-    model.train()
-    total_loss = 0 
-    for batch, (src, tgt) in enumerate(dataloader):
-        # print(f'batch: {batch}/{total_batches}')
-        src, tgt = src.to(device), tgt.to(device)
-        optimizer.zero_grad()
-        logits = model(src, tgt)
-
-
-        # TODO: Understand a bit better the shapes and loss calcs
-        loss = criterion(logits.view(-1, logits.size(-1)), tgt.view(-1))
-
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    epoch_time = time.time() - start_time
-    epoch_times.append(epoch_time)
-    print('epoch:', epoch, 'loss:', total_loss, 'time:', epoch_time)
-
-torch.save(model.state_dict(), model_output_path)
